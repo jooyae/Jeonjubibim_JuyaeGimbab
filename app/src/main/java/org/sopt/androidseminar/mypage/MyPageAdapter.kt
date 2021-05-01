d25d7b7c5d8ee5ac3f0ac7260ecc22d02d8bf8d5
JooYae <jooyae3970@naver.com>
2021-05-01 19:44:04 +0900

    viewpager2 viewholder

--- app/src/main/java/org/sopt/androidseminar/mypage/MyPageAdapter.kt
+++ app/src/main/java/org/sopt/androidseminar/mypage/MyPageAdapter.kt
@@ -1,27 +1,37 @@
 package org.sopt.androidseminar.mypage
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.androidseminar.databinding.ActivityMyPageBinding
+import org.sopt.androidseminar.databinding.ItemMypageBinding
 import org.sopt.androidseminar.mypage.data.IntroData
 
 class MyPageAdapter : RecyclerView.Adapter<MyPageAdapter.MyPageViewHolder>(){
-    private val introList = mutableListOf<IntroData>()
-
-    class MyPageViewHolder(private val binding : ActivityMyPageBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-                fun bind(introList : IntroData){
-
-                }
-            }
+    private val introData = mutableListOf<IntroData>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemMypageBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MyPageViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyPageViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(introData[position])
     }
 
     override fun getItemCount(): Int = 3
+
+    class MyPageViewHolder(
+        private val binding : ItemMypageBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(introData: IntroData) {
+            binding.textviewTitleMypage.text = introData.textview_Title
+            binding.textviewSubtitleMypage.text = introData.textview_Subtitle
+        }
+    }
 }
